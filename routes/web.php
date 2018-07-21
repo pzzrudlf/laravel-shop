5329062a1e36742d3941f08415fac28847788ac9
ABCDXIAOXIAOSASA <1252805071@qq.com>
2018-07-21 18:42:31 +0800

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+});
