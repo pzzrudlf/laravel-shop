297f6e4a04d46cce7583a2e9d652641fe4a6c92d
pzzrudlf <pzzrudlf@google.com>
2018-07-28 16:12:10 +0800

    modify goods model

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -30,39 +30,6 @@ class ProductsController extends Controller
     }
 
     /**
-     * Edit interface.
-     *
-     * @param $id
-     * @return Content
-     */
-    public function edit($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
-
-            $content->header('header');
-            $content->description('description');
-
-            $content->body($this->form()->edit($id));
-        });
-    }
-
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
-    public function create()
-    {
-        return Admin::content(function (Content $content) {
-
-            $content->header('header');
-            $content->description('description');
-
-            $content->body($this->form());
-        });
-    }
-
-    /**
      * Make a grid builder.
      *
      * @return Grid
@@ -94,6 +61,24 @@ class ProductsController extends Controller
     }
 
     /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function(Content $content) {
+            $content->header('创建商品');
+            $content->body($this->form());
+        });
+    }
+
+    public function store()
+    {
+        return $this->form()->store();
+    }
+
+    /**
      * Make a form builder.
      *
      * @return Form
@@ -102,10 +87,39 @@ class ProductsController extends Controller
     {
         return Admin::form(Product::class, function (Form $form) {
 
-            $form->display('id', 'ID');
+            $form->text('title', '商品名称')->rules('required');
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->image('image', '封面图片')->rules('required|image');
+
+            $form->editor('description', '商品描述')->rules('required');
+
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+
+            $form->hasMany('skus', 'SKU 列表', function(Form\NestedForm $form) {
+               $form->text('title', 'SKU 名称')->rules('required');
+               $form->text('description', 'SKU 描述')->rules('required');
+               $form->text('price', '单价')->rules('required|numeric|min:0.01');
+               $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
+
+            $form->saving(function (Form $form){
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            });
+
         });
     }
+
+    public function edit($id)
+    {
+        return Admin::content(function(Content $content) use ($id) {
+            $content->header('编辑商品');
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    public function update($id)
+    {
+        return $this->form()->update($id);
+    }
+
 }
